8b63728a7d804f3257f7087125b9639cae2111d9
Nicolas Linotte <nilinott@z4r9p5.42lyon.fr>
2026-02-22 18:44:54 +0100

    [mod] finish script python

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -10,29 +10,47 @@
 #                                                                             #
 # *************************************************************************** #
 
-class Plant():
-    def __init__(self, name: str, height: int, age: int):
+class SecurePlant():
+    def __init__(self, name: str):
         self.__name = name
-        self.__height = height
-        self.__age = age
+        self.__height = 0
+        self.__age = 0
+        print(f"Plant created: {self.__name}")
 
     def set_height(self, height: int) -> None:
-        self.height = height
+        if height < 0:
+            print(f"Invalid operation attempted: height {height}cm [REJECTED]")
+            print("Security: Negative height rejected\n")
+        else:
+            self.__height = height
+            print(f"Height updated: {height}cm [OK]")
 
     def get_height(self):
-        return self.height
+        return self.__height
+
+    def get_age(self):
+        return self.__age
+
+    def n(self):
+        return self.__name
 
     def set_age(self, age: int) -> None:
-        self.age = age
+        if age < 0:
+            print(f"Invalid operation attempted: age {age}cm [REJECTED]")
+            print("Security: Negative height rejected")
+        else:
+            self.__age = age
+            print(f"Age updated: {age} days [OK]\n")
 
 
 def main() -> None:
-    plants = [
-        Plant("Rose", 25, 30),
-    ]
-
-    plants[0].set_height(58)
     print("=== Garden Security System ===")
+    p = SecurePlant("Rose")
+    p.set_height(25)
+    p.set_age(30)
+
+    p.set_height(-5)
+    print(f"Current plant: {p.n()} ({p.get_height()}cm, {p.get_age()} days)")
 
 
 if __name__ == "__main__":
