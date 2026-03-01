5e3023747153b27bdfa7f5895ad34ed11b4c2497
nicolasl2003 <nicolaslinotte84@gmail.com>
2026-03-01 23:13:59 +0100

    [mod] finish exercice

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -11,45 +11,97 @@
 # *************************************************************************** #
 
 class Plant():
-    def __init__(self, name: str, height: int, age: int, typep: str):
+    def __init__(self, name: str, height: int, age: int):
         self.__name = name
         self.__height = height
         self.__age = age
 
+    def name(self):
+        return (self.__name)
+
+    def height(self):
+        return (self.__height)
+
+    def age(self):
+        return (self.__age)
+
 
 class Flower(Plant):
-    def __init__(self, name: str, height: int, age: int, color: str, bloom, typep: str):
-        super().__init__(name, height, age, typep)
+    def __init__(self, name: str, height: int, age: int, color: str, bloom):
+        super().__init__(name, height, age)
         self.color = color
         self.bloom = bloom
 
-        if (bloom == True):
-            #print(f"{self.__name} is blooming beautifully!")
-            return
+    def get_bloom(self):
+        if (self.bloom):
+            return f"{self.name()} is blooming beautifully!\n"
+        return "\n"
+
+    def get_color(self):
+        return f"{self.color} color"
+
+    def get_plant(self):
+        return (f"{self.name()} (Flower): "
+                f"{self.height()}cm, {self.age()} days, {self.get_color()}\n"
+                f"{self.get_bloom()}")
 
 
-#class Tree(Plant):
- #   def __init__(self, name: str, height: int, age: int, ):
+class Tree(Plant):
+    def __init__(self, name: str, height: int, age: int, diameter: int):
+        super().__init__(name, height, age)
+        self.diameter = diameter
+
+    def produce_shade(self):
+        result: int = (self.height() * self.diameter)
+        return f"{self.name()} provides {result} square meters of shade\n"
+
+    def trunk_diameter(self):
+        return f"{self.diameter} diameter"
+
+    def get_plant(self):
+        return (
+            f"{self.name()} (Vegetable): "
+            f"{self.height()}cm, {self.age()} days, {self.trunk_diameter()}\n"
+            f"{self.produce_shade()}")
 
 
-def display_plant(plant):
+class Vegetable(Plant):
+    def __init__(self, name: str, height: int, age: int):
+        super().__init__(name, height, age)
+
+    def harvest_season(self):
+        return "summer harvest"
+
+    def nutritional_value(self):
+        return f"{self.name()} is rich in vitamin C"
+
+    def get_plant(self):
+        return (
+            f"{self.name()} (Tree): "
+            f"{self.height()}cm, {self.age()} days, {self.harvest_season()}\n"
+            f"{self.nutritional_value()}")
+
+
+def display_plant(plants):
     i: int
     max_plant: int
 
     i = 0
-    max_plant = len(plant)
+    max_plant = len(plants)
 
     while (i < max_plant):
-        print()
+        print(f"{plants[i].get_plant()}")
         i += 1
 
 
 def main() -> None:
-    print("=== Garden Plant Types ===")
-    plant = {
-        Flower("Rose", 10, 20, "rouge", True, "flower")
-    }
-    display_plant(plant)
+    print("=== Garden Plant Types ===\n")
+    plants = [
+        Flower("Rose", 10, 20, "red", True),
+        Tree("Oak", 500, 1828, 50),
+        Vegetable("Tomato", 80, 90),
+    ]
+    display_plant(plants)
 
 
 if __name__ == "__main__":
